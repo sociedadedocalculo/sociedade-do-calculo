0a1dcd807e3c463c054ff68a5a778af8d95167de
Patrick Garcia <patrick.garcia@aluno.poa.ifrs.edu.br>
2018-09-26 19:04:11 -0300

    Atualizando cs  de conexao

---
+++ Assets/Plugins/ConexaoDB.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using Mono.Data.SqliteClient;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConexaoDB : MonoBehaviour {
+    private IDbConnection conexao;
+    private IDbComand comando;
+    private IDbDataReader reader;
+
+
+    private
+
+public  void Start() {
+    Connection();
+}
+
+
+public  void Connection() {
+    
+}
+
+public  void Inserir() {
+    
+}
+
+public  void AtualizarColuna() {
+    
+}
+
+public  void Atualizar() {
+    
+}
+
+public  void Consultar() {
+    
+}
+
+public  void ConsultaCompleta() {
+    
+}
+
+
+}
