3c0d51c4a8f0100b053378d422ee84d3c5a803d2
Patrick Garcia <patrick.garcia@aluno.poa.ifrs.edu.br>
2018-11-24 07:36:33 -0200

    Animations

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 20.0f;
+    private Vector3 pos; 
+
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = transform.position;
+        
+        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        pos.y += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.position = pos;
+    }
+
+ 
+
+}
