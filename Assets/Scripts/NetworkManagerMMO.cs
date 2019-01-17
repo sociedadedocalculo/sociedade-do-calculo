2d05e6db176bac0dcb94611f6c2ae1311c3dc0c0
Patrick Garcia <patrickvicentegarcia@gmail.com>
2019-01-17 20:25:29 -0200

    atualizando arquivos e assets

--- Assets/Scripts/NetworkManagerMMO.cs
+++ Assets/Scripts/NetworkManagerMMO.cs
@@ -347,13 +347,11 @@ public partial class NetworkManagerMMO : NetworkManager
     {
         selection = -1;
         foreach (Transform location in selectionLocations)
-        {
             if (location.childCount > 0)
-            {
                 Destroy(location.GetChild(0).gameObject);
-            }
-        }
     }
+
+
 
     void OnClientCharactersAvailable(NetworkMessage netMsg)
     {
