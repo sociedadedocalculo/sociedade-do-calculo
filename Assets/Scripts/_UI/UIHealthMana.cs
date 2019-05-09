a73f7bb5557a4d880f353a98579b95286f3e5b40
Patrick Garcia <patrickvicentegarcia@gmail.com>
2019-05-09 15:43:47 -0300

    conf assets da cena e terrains

---
+++ Assets/Scripts/_UI/UIHealthMana.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public partial class UIHealthMana : MonoBehaviour
+{
+    public GameObject panel;
+    public Slider healthSlider;
+    public Text healthStatus;
+    public Slider manaSlider;
+    public Text manaStatus;
+
+    void Update()
+    {
+        Player player = Utils.ClientLocalPlayer();
+        panel.SetActive(player != null); // hide while not in the game world
+        if (!player) return;
+
+        healthSlider.value = player.HealthPercent();
+        healthStatus.text = player.health + " / " + player.healthMax;
+
+        manaSlider.value = player.ManaPercent();
+        manaStatus.text = player.mana + " / " + player.manaMax;
+    }
+}
