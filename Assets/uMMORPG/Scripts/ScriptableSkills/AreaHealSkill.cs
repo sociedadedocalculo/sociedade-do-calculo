7ef15b084093beb2b9b9fa9d6af8293c289f6e6e
Patrick Garcia <patrickvicentegarcia@gmail.com>
2019-09-13 20:09:09 -0300

    ajustes projetos

---
+++ Assets/uMMORPG/Scripts/ScriptableSkills/AreaHealSkill.cs
@@ -0,0 +1,57 @@
+﻿// Group heal that heals all entities of same type in cast range
+// => player heals players in cast range
+// => monster heals monsters in cast range
+
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+[CreateAssetMenu(menuName="uMMORPG Skill/Area Heal", order=999)]
+public class AreaHealSkill : HealSkill
+{
+    public override bool CheckTarget(Entity caster)
+    {
+        // no target necessary, but still set to self so that LookAt(target)
+        // doesn't cause the player to look at a target that doesn't even matter
+        caster.target = caster;
+        return true;
+    }
+
+    public override bool CheckDistance(Entity caster, int skillLevel, out Vector3 destination)
+    {
+        // can cast anywhere
+        destination = caster.transform.position;
+        return true;
+    }
+
+    public override void Apply(Entity caster, int skillLevel)
+    {
+        // candidates hashset to be 100% sure that we don't apply an area skill
+        // to a candidate twice. this could happen if the candidate has more
+        // than one collider (which it often has).
+        HashSet<Entity> candidates = new HashSet<Entity>();
+
+        // find all entities of same type in castRange around the caster
+        Collider[] colliders = Physics.OverlapSphere(caster.transform.position, castRange.Get(skillLevel));
+        foreach (Collider co in colliders)
+        {
+            Entity candidate = co.GetComponentInParent<Entity>();
+            if (candidate != null &&
+                candidate.health > 0 && // can't heal dead people
+                candidate.GetType() == caster.GetType()) // only on same type
+            {
+                candidates.Add(candidate);
+            }
+        }
+
+        // apply to all candidates
+        foreach (Entity candidate in candidates)
+        {
+            candidate.health += healsHealth.Get(skillLevel);
+            candidate.mana += healsMana.Get(skillLevel);
+
+            // show effect on candidate
+            SpawnEffect(caster, candidate);
+        }
+    }
+}
---
+++ Assets/uMMORPG/Scripts/SkillEffects/OneTimeTargetSkillEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class OneTimeTargetSkillEffect : SkillEffect
+{
+    void Update()
+    {
+        // follow the target's position (because we can't make a NetworkIdentity
+        // a child of another NetworkIdentity)
+        if (target != null)
+            transform.position = target.collider.bounds.center;
+
+        // destroy self if target disappeared or particle ended
+        if (isServer)
+            if (target == null || !GetComponent<ParticleSystem>().IsAlive())
+                NetworkServer.Destroy(gameObject);
+    }
+}
